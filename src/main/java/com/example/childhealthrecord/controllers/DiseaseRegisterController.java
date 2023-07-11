47b677087c18cd18b77b9ae6c56cf2651b1178db
Katarzyna <noworyta.katarzyna@wp.pl>
2023-07-11 11:19:49 +0200

    layout

---
+++ src/main/java/com/example/childhealthrecord/controllers/DiseaseRegisterController.java
@@ -0,0 +1,19 @@
+package com.example.childhealthrecord.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DiseaseRegisterController {
+
+    @Value("${app.title.diseaseRegister}")
+    private String title;
+
+    @GetMapping("/diseaseRegister")
+    public String diseaseRegisterPage(Model model){
+        model.addAttribute("title", title);
+        return "diseaseRegister";
+    }
+}
