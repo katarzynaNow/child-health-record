f5eec449b3aaabc35624661c914d98fe4fa09279
Katarzyna <noworyta.katarzyna@wp.pl>
2023-08-11 12:42:14 +0200

    improvements

--- src/main/java/com/example/childhealthrecord/entity/Symptom.java
+++ src/main/java/com/example/childhealthrecord/entity/Symptom.java
@@ -4,10 +4,12 @@ public enum Symptom {
     CHILLS ("chills"),
     COUGH ("cough"),
     DIARRHEA ("diarrhea"),
+    EAR_ACHE ("ear ache"),
     FEVER ("fever"),
     RASH ("rash"),
     RUNNY_NOSE ("runny nose"),
     SORE_THROAT ("sore throat"),
+    STOMACHACHE ("stomachache"),
     VOMITING ("vomiting");
 
     private String name;
